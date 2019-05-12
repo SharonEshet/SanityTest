9919d402c146e73b99e379b493822bb9a00f3f18
SharonElia123 <sharonhili169@gmail.com>
2019-05-12 12:04:26 +0300

    Update CheckDolarInFlight.java
    Change Func

--- CheckDolarInFlight.java
+++ CheckDolarInFlight.java
@@ -14,23 +14,38 @@ import java.util.concurrent.TimeUnit;
 
 public class CheckDolarInFlight extends Testsh {
     @Before
-    public void openwindow() throws InterruptedException, FindFailed {
-        openChrome();
-        }
+     public void openwindow() throws InterruptedException, FindFailed {
 
+        openChrome();
+    }
     @Test
     public void test4() throws InterruptedException, FindFailed {
-        driver.get(test1+"internationalflights/searchresults/?page=1&fClass=Y&maxWaitingTime=0&stopsNumber&ways=2&origin=TLV&destination=TSR&from1=&to1=&from2=&to2=&from3=&from=18/10/2018&fromHours=כל+השעות&to=25/10/2018&toHours=כל+השעות&firstDate=18/10/2018&firstDateTime=כל+השעות&secondDate=&thirdDate=25/10/2018&thirdDateTime=כל+השעות&adult=2&child=0&baby=0&young=0&old=0&amount=393&airline=W6&directFlights=True&calendarOnly=false&isPromotion=5");
-        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        Thread.sleep(2000L);
-        WebElement element1 = driver.findElement(By.xpath("//*[@id=\"results\"]/div/div/div[2]/div[2]/span"));
-        String actualValue = element1.getText();
-        Assert.assertEquals("$", actualValue);
-        System.out.print("Great!!"+ " " + actualValue);
-         Thread.sleep(2000L);
-     //   driver.close();
+        Thread.sleep(2000L)
+        CheckAsseration();
+    }
 
+    public void CheckAsseration() throws InterruptedException, FindFailed {
+        // Check Headlines
+        WebElement element1 = driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div[1]/header/div/div[1]/div/div/ul/li[4]/div[1]"));
+        String actualValue = element1.getText();
+        Assert.assertEquals("חופשה בארץ", actualValue);
+        //Details to back- sh
+        WebElement element3 = driver.findElement(By.xpath("//*[@class='contact-us-greet']"));
+        String actualValue2 = element3.getText();
+        Assert.assertEquals("הנכם מוזמנים לפנות לנציגי המכירות והשירות בכל נושא ואנו נחזור אלכם בהקדם", actualValue2);
+        WebElement element4 = driver.findElement(By.xpath("//*[@class='abroad-phone-text two-phones-number']"));
+        String actualValue3 = element4.getText();
+        //Check Phone Number
+        Assert.assertEquals("03-7771111", actualValue3);
+        WebElement element5=driver.findElement(By.xpath("//*[@class='domestic-phone-text two-phones-number']"));
+        String actualValue4 = element5.getText();
+        Assert.assertEquals("03-7771177", actualValue4);
+        Thread.sleep(2000L);
+        driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div[2]/div[1]/div/div[2]/div[1]/div[1]/article/div[1]/img")).click();
+        Thread.sleep(2000L);
+        driver.close();
     }}
+
 
 
