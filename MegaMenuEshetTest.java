50a4af52bb3a14f7e18e18b89ae11c9f634e09e3
SharonElia123 <sharonhili169@gmail.com>
2019-05-12 14:53:28 +0300

    Change Func
    //

--- MegaMenuEshetTest.java
+++ MegaMenuEshetTest.java
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 public class MegaMenuEshetTest extends Testsh{
     @Before
-    public void openwindow() throws InterruptedException {
+   // public void openwindow() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:/SeleniumDrivers/Java/Driver/update/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get(test1);
