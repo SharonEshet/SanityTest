40ceb0ff5650d52cf0e348cc89f94da2475b9f83
SharonElia123 <sharonhili169@gmail.com>
2019-05-12 11:45:03 +0300

    Update CheckDolarInFlight.java
    Func fix

--- CheckDolarInFlight.java
+++ CheckDolarInFlight.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class CheckDolarInFlight extends Testsh {
 
     @Before
-     public  void 
+    public void openwindow() throws InterruptedException, FindFailed {
         openChrome();
         }
 
